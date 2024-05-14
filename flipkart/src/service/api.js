86772d446c7b2ca3659b3448f9a4d7ab4efa9c10
Kamrul Hasan <38207177+kamrul9670@users.noreply.github.com>
2024-05-14 10:42:42 +0530

    Update api.js

--- flipkart/src/service/api.js
+++ flipkart/src/service/api.js
@@ -2,7 +2,7 @@
 
      import axios from 'axios';
 
-       const URL = 'deploy-mern-api-eight.vercel.app
+       const URL = 'http://deploy-mern-api-eight.vercel.app
 ';
 
      export const authenticateSignup  = async (data)  =>  {
